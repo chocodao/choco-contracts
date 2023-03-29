93d80730ca6663e3b73e2acdc608d872b38cf0a8
Choco Finance <grahammckee@protonmail.com>
2023-03-28 22:47:30 -0500

    add DAO contract @gnosis

--- DAOMultiSig.sol
+++ DAOMultiSig.sol
@@ -20,7 +20,7 @@ pragma solidity ^0.4.15;
 
 
 /// @title Multisignature wallet - Allows multiple parties to agree on transactions before execution.
-/// @author Stefan George - <stefan.george@consensys.net>
+/// @author Stefan George - <stefan.george@consensys.net> 
 contract MultiSigWallet {
 
     /*
